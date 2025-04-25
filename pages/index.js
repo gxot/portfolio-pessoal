dc75d33fcac8120b179364160c60b363a0fad97a
gxot <gustschott@gmail.com>
2025-04-25 16:51:44 -0300

    Alteração do Layout

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="flex flex-col max-w-xl mx-auto mt-10 items-center justify-center text-center">
         <img src="/img/eu.png" className="w-70 h-70 mx-auto object-cover rounded-full shadow-xl"></img>
         <h1 className="font-bold text-white text-4xl mx-auto pt-5 shadow-xl">Gustavo Henrique Schott</h1>
-        <h3 className="text-white font-semibold text-sm mx-auto pt-3">Estudante de Sistemas de Informação (PUCPR) com 20 anos de idade e foco em backend e ciência de dados. Tenho conhecimento em Python  e Java, e estou aprendendo desenvolvimento web com Javascript e CSS (Preferência por Next.js e Tailwind CSS em projetos pessoais). Estou em busca da minha primeira oportunidade na área de T.I</h3>
+        <h3 className="text-white font-semibold text-sm mx-auto pt-3">Estudante de Sistemas de Informação (PUCPR) com 20 anos de idade e foco em backend e ciência de dados. Tenho conhecimento em Python, Java e MySQL, e estou desenvolvendo minhas habilidades front-end com Javascript, HTML e CSS (Preferência por Next.js e Tailwind CSS em projetos pessoais). Estou em busca da minha primeira oportunidade na área de T.I</h3>
       </div>
       <Contatos />
       <Languages />
