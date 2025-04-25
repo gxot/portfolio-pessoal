8633b1a4d07d4205e58b0139ab60b3a14fa61377
gxot <gustschott@gmail.com>
2025-04-25 05:52:22 -0300

    Alteração no texto

--- components/LeitorCSV.jsx
+++ components/LeitorCSV.jsx
@@ -20,7 +20,7 @@ export default function LeitorCSV() {
                             <h3 className="text-center font-semibold mb-2">Leitor CSV</h3>
                             <img src="/img/leitorcsv.png" className="h-[50%] w-full object-cover rounded-2xl mb-2 mx-auto" />
                             <h3 className='mx-auto font-bold'>Link: </h3>
-                            <a href='https://github.com/gxot/leitorcsv' className="mx-auto" target="_blank">https://github.com/gxot/leitorcsv</a>
+                            <a href='https://github.com/gxot/leitorcsv' className="mx-auto text-blue" target="_blank">https://github.com/gxot/leitorcsv</a>
                             <h3 className="text-center text-black font-bold mt-4">Descrição: </h3>
                             <p>Uma aplicação web utilizando Next.js e Tailwind CSS feita para abrir arquivos CSV direto do navegador. O usuário só precisa fazer o upload do arquivo, então você pode selecionar o tipo de disposição de dados e os dados aparecem organizados em uma tabela. É uma solução simples, mas útil pra quem quer visualizar rapidamente o conteúdo de planilhas sem depender de outros programas.</p>
                             <button type="button" onClick={() => setIsOpen(false)} className='mt-auto mx-auto rounded-xl w-20 bg-red-600 text-white hover:scale-120 cursor-pointer'>Fechar</button>
--- components/WebScraperOlx.jsx
+++ components/WebScraperOlx.jsx
@@ -18,9 +18,9 @@ export default function LeitorCSV() {
                     <div className='absolute inset-0 bg-black opacity-50'></div>
                         <div className='relative flex flex-col bg-white w-[90%] h-[80%] max-w-3xl p-6 rounded-xl shadow-lg'>
                             <h3 className="text-center font-semibold mb-2">Web Scraper OLX</h3>
-                            <img src="/img/leitorcsv.png" className="h-[50%] w-full object-cover rounded-2xl mb-2 mx-auto" />
+                            <img src="/img/webscraperolx.png" className="h-[50%] w-full object-cover rounded-2xl mb-2 mx-auto" />
                             <h3 className='mx-auto font-bold'>Link: </h3>
-                            <a href='https://github.com/gxot/web-scraper-olx' className="mx-auto" target="_blank">https://github.com/gxot/web-scraper-olx</a>
+                            <a href='https://github.com/gxot/web-scraper-olx' className="mx-auto text-blue" target="_blank">https://github.com/gxot/web-scraper-olx</a>
                             <h3 className="text-center text-black font-bold mt-4">Descrição: </h3>
                             <p>Esse projeto une automação com web e dados — três coisas que eu curto bastante. Ele permite buscar produtos na OLX digitando o nome e escolhendo o estado. O sistema usa Selenium pra fazer o scraping dos anúncios, organiza tudo com Pandas e mostra os resultados em uma interface feita com Next.js e Tailwind CSS. Cada item tem nome, preço e link direto pro anúncio.</p>
                             <button type="button" onClick={() => setIsOpen(false)} className='mt-auto mx-auto rounded-xl w-20 bg-red-600 text-white hover:scale-120 cursor-pointer'>Fechar</button>
